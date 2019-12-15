28ed81dcaf5b75cd498b35a1d17584519ec7ee2d
woehd9410 <woehd9410@gmail.com>
2019-12-15 23:56:41 +0900

    [Title : 주사위 윷놀이 | map 활용 DFS 문제]

--- Baekjoon/17825.cpp
+++ Baekjoon/17825.cpp
@@ -1,164 +1,113 @@
-/*
-	2019-12-14 Saturday
-	BaekJoon ªÔº∫ SW ±‚√‚πÆ¡¶
-	¡÷ªÁ¿ß ¿∑≥Ó¿Ã
-*/
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#define endl '\n'
+#include <map>
+#include <vector>
 
 using namespace std;
 
+#define endl '\n'
 
-int answer;
-pair<int, int> horse[4];
-bool goal[4];
-int arr[11];
-bool visit[42];
-int line1[] = { 0, 13, 16, 19, 25, 30, 35, 40, 42};
-int line2[] = { 0, 22, 24, 25, 30, 35, 40, 42};
-int line3[] = { 0, 28, 27, 26, 25, 30, 35, 40, 42};
+int dice[10];
+int ans;
 
+map<int, int> red = {
+	{0, 2}, {2, 4} , {4, 6}, {6, 8}, {8, 10},
+	{10, 12}, {12, 14}, {14, 16}, {16, 18}, {18, 20},
+	{20, 22}, {22, 24}, {24, 26}, {26, 28}, {28, 30},
+	{30, 32}, {32, 34}, {34, 36}, {36, 38}, {38, 40}, {40, 50}
+};
 
+map<int, int> blue = {
+	{10, 13}, {13, 16}, {16, 19}, {19, 25},
+	{20, 22}, {22, 24}, {24, 25}, {30, 28},
+	{28, 27}, {27, 26}, {26, 25}
+};
+
+map<int, int>blue_goal = {
+	{25, 30}, {30, 35}, {35, 40}, {40, 50}
+};
 
 void Input() {
 	for (int i = 0; i < 10; i++) {
-		cin >> arr[i];
-	}
-	for (int i = 0; i < 4; i++) {
-		horse[i].first = 0;
-		horse[i].second = 0;
+		cin >> dice[i];
 	}
 }
 
+int move(int idx, int now, vector<pair<int, int>>& locate) {
+	int present = locate[idx].first;
+	int dir = locate[idx].second;
+	int cnt = dice[now];
 
-void DFS(int horseNum, int now, int sum) {
-	if (now == 10) {
-		if (answer < sum) {
-			answer = sum;
-			cout << answer << endl;
-		}
-		return;
-	}
-	int line = horse[horseNum].second;
-	if (horse[horseNum].second == 0) {
-		if ((horse[horseNum].first * 2) % 10 == 0) {
-			horse[horseNum].second = horse[horseNum].first * 2 / 10;
-			horse[horseNum].first = 0;
-		}
-	}
-	int cnt = arr[now]; int start = horse[horseNum].first;
-	int plus = 0;
-	int move = 0;
-	if (horse[horseNum].second == 0) {
-		if (start * 2 == 42) return;
-		while (cnt--) {
-			move++;
-			if ((start + move) * 2 == 42) {
-				visit[start * 2] = false;
-				horse[horseNum].first = start + move;
-				return;
-			}
-			if (visit[(start + move) * 2]) {
-				move = 0;
-				return;
-			}
-		}
-		plus = (start + move) * 2;
-		sum += plus;
-		visit[horse[horseNum].first * 2] = false;
-		horse[horseNum].first = start + move;
-		visit[(start + move) * 2] = true;
-	}
-	else if (horse[horseNum].second == 1) {
-		if (line1[start] == 42) return;
-		while (cnt--) {
-			move++;
-			if (line1[start + move] == 42) {
-				visit[line1[horse[horseNum].first]] = false;
-				horse[horseNum].first = start + move;
-				return;
-			}
-			if (visit[line1[start + move]]) {
-				return;
-			}
-		}
-		plus = line1[start + move];
-		sum += plus;
-		visit[line1[horse[horseNum].first]] = false;
-		horse[horseNum].first = start + move;
-		visit[start + move] = true;
-	}
-	else if (horse[horseNum].second == 2) {
-		if (line2[start] == 42) return;
-		while (cnt--) {
-			move++;
-			if (line2[start + move] == 42) {
-				visit[line2[horse[horseNum].first]] = false;
-				horse[horseNum].first = start + move;
-				return;
-			}
-			if (visit[line2[start + move]]) {
-				return;
-			}
-		}
-		plus = line2[start + move];
-		sum += plus;
-		visit[line2[horse[horseNum].first]] = false;
-		horse[horseNum].first = start + move;
-		visit[start + move] = true;
-	}
-	else if (horse[horseNum].second == 3) {
-		if (line3[start] == 42) return;
-		while (cnt--) {
-			move++;
-			if (line3[start + move] == 42) {
-				visit[line3[horse[horseNum].first]] = false;
-				horse[horseNum].first = start + move;
-				return;
-			}
-			if (visit[line3[start + move]]) {
-				return;
-			}
-		}
-		plus = line3[start + move];
-		sum += plus;
-		visit[line3[horse[horseNum].first]] = false;
-		horse[horseNum].first = start + move;
-		visit[start + move] = true;
-	}
-	cout << sum << endl;
+	for (int i = 0; i < cnt; i++) {
+		if (dir == 0) present = red[present];
+		if (dir == 1) present = blue[present];
+		if (dir == 2) present = blue_goal[present];
 
+		if (present == 25) dir = 2;
+		else if (present == 50) {
+			locate[idx].first = present;
+			return 0;
+		}
+	}
+
+	if (dir == 0 && (present == 10 || present == 20 || present == 30)) {
+		dir = 1;
+	}
+	bool ok = true;
 	for (int i = 0; i < 4; i++) {
 		
-		
-		DFS(i, now + 1, sum);
-		sum -= plus;    
-		if (horse[horseNum].second == 0) {
-			visit[(start + move) * 2] = false;
-		}
-		visit[start + move] = false;
-		horse[horseNum].first -= move;
-		horse[horseNum].second = line;
+		if (idx == i) continue;
+		if (present == locate[i].first &&
+			(present == 40 || dir == locate[i].second)) ok = false;
+	}
+
+	if (ok) {
+		locate[idx].first = present;
+		locate[idx].second = dir;
+		return present;
+	}
+	else {
+		return -1;
 	}
 	
+	return 0;
 }
 
-void Solve() {
-	answer += arr[0] * 2;
-	int sum = answer;
-	horse[0].first = arr[0];
-	visit[2 * arr[0]] = false;
-	int line = 0;
-	DFS(0, 1 , sum);
+void DFS(int now, int sum, vector<pair<int, int>> &locate) {
+	if (now == 10) {
+		if (ans < sum) ans = sum;
+		return;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		int prev_locate = locate[i].first;
+		int prev_dir = locate[i].second;
+
+		if (prev_locate == 50) continue;
+		int ret = move(i, now, locate);
+		if (ret > -1) {
+			DFS(now + 1, sum + ret, locate);
+		}
+
+		locate[i].first = prev_locate;
+		locate[i].second = prev_dir;
+	}
 }
 
 int main() {
 	freopen("Text.txt", "r", stdin);
 	Input();
 
-	Solve();
-	cout << "¡§¥‰ : ";
-	cout << answer << endl;
+	vector<pair<int, int>> locate =
+	{
+		{0, 0},
+		{0, 0},
+		{0, 0},
+		{0, 0}
+	};
+
+	DFS(0, 0, locate);
+
+	cout << ans << endl;
 }
