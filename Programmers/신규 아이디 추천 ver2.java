4ad733890e0b8f880bb562c4ff8303ae3b46f3a0
Park Jae Dong <woehd9410@gmail.com>
2021-08-04 23:54:58 +0900

    [Title : 신규 아이디 추천 | replaceAll 활용 ]

---
+++ Programmers/신규 아이디 추천 ver2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String solution(String new_id) {
+            String answer = "";
+        //1단계
+        answer = new_id.toLowerCase();
+        
+        //2단계
+        answer = answer.replaceAll("[^-_.a-z0-9]", "");
+        
+        //3단계
+        answer = answer.replaceAll("[.]{2,}", ".");
+        
+        //4단계
+        answer = answer.replaceAll("^[.]|[.]$", "");
+        
+        //5단계
+        if(answer.length() == 0) answer = "a";
+        
+        //6단계
+        if(answer.length() >= 16) {
+        	answer = answer.substring(0,15);
+        	answer = answer.replaceAll("[.]$", "");
+        }
+        
+        //7단계
+        if(answer.length() <= 2) {
+        	while(answer.length() < 3) {
+        		answer += answer.charAt(answer.length() - 1);
+        	}
+        }
+        return answer;
+    }
+}
